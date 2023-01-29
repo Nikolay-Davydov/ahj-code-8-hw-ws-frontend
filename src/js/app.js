5035bce790edfeb363d1e2114736382b85440c43
Nikolay-Davydov <davidovns70@gmail.com>
2023-01-29 15:45:57 +0100

    init

--- src/js/app.js
+++ src/js/app.js
@@ -1,46 +1,9 @@
-import goblinImg from './../img/goblin.png'
+import Board from "./board";
+import Goblin from "./goblin";
+import GamePlay from "./gamePlay";
 
-class Game {
-  constructor() {
-    this.previosCord = 0;
-    this.counter = 0;
-    this.field = document.querySelectorAll(".game_field");
-  }
+const board = new Board();
+const char = new Goblin();
+const gameplay = new GamePlay(board, char);
 
-  activeImg() {
-    const img = document.createElement("img");
-    img.src = goblinImg;
-    return img;
-  }
-
-  dellImg() {
-    const img = document.querySelector("img");
-    if (img) {
-      img.remove();
-    }
-  }
-
-  randomNumber(maximum) {
-    return Math.floor(Math.random() * maximum);
-  }
-
-  randomInsert() {
-    const cordField = this.randomNumber(this.field[0].children.length);
-    if (cordField == this.previosCord) {
-      this.randomInsert();
-    } else {
-      this.previosCord = cordField;
-      this.field[0].children[cordField].appendChild(this.activeImg());
-    }
-  }
-
-  main() {
-    this.dellImg();
-    this.randomInsert();
-    this.counter += 1;
-  }
-}
-
-const newGame = new Game();
-
-setInterval(() => newGame.main(), 1000);
+gameplay.init();
