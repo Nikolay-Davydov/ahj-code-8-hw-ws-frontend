fd00e5873e41c44512ef6057425484b5943c7fa7
Nikolay-Davydov <davidovns70@gmail.com>
2023-04-17 21:14:31 +0200

    init

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-import "./css/style.css";
+import './css/style.css';
 
-import "./js/app";
+import './js/app.js';
+
+// TODO: write your code in app.js
--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,5 @@
-import { Tooltip } from "./tooltip";
+/* eslint-disable import/extensions */
+import Widget from './Widget';
 
-const button = document.querySelector(".btn");
-
-const tooltip = new Tooltip();
-
-button.addEventListener("click", () => {
-  if (document.querySelector(".tooltip-wrapper") === null) {
-    tooltip.addTooltip(button);
-  } else {
-    tooltip.removeTooltip();
-  }
-});
+const helpDesk = new Widget(document.querySelector('.container'));
+helpDesk.bindToDOM();
